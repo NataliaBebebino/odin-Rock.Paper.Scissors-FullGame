d50f99f5052b9e4b68ea60d68d268d2c5fb5723d
NataliaBebebino <nbebebino@gmail.com>
2022-06-24 20:02:06 -0400

    Refactor JavaScript code

--- javascript/game.js
+++ javascript/game.js
@@ -5,58 +5,61 @@ let counter = {
     computer : 0
 };
 
-let btnPlayerElement = document.querySelectorAll(".player-game-element");
-let btnComputerElement = document.querySelectorAll(".computer-game-element");
-let playerScore = document.querySelector("#player-score");
-let computerScore = document.querySelector("#computer-score");
-let individualRoundResult = document.querySelector("#individual-round-result");
-let btnPlayAgain = document.querySelector("#btn-play-again");
-let finalResult = document.querySelector("#final-result");
+const rockPlayerElement = document.getElementById("rock-player-element");
+const paperPlayerElement = document.getElementById("paper-player-element");
+const scissorsPlayerElement = document.getElementById("scissors-player-element");
+const btnComputerElement = document.querySelectorAll(".computer-game-element");
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
+const individualRoundResult = document.querySelector("#individual-round-result");
+const btnPlayAgain = document.querySelector("#btn-play-again");
+const finalResult = document.querySelector("#final-result");
 
 
-for (let index = 0; index < btnPlayerElement.length; index++) {
-    btnPlayerElement[index].addEventListener("click", playRound);
-}
+rockPlayerElement.addEventListener("click", () => playRound("rock"));
+paperPlayerElement.addEventListener("click", () => playRound("paper"));
+scissorsPlayerElement.addEventListener("click", () => playRound("scissors"));
+btnPlayAgain.addEventListener("click", playAgain);
 
-function playRound(e) {
-    let playerSelection = e.target.innerText.toLowerCase();
+
+function playRound(playerSelection) {
     let computerSelection = gameElements[Math.floor(Math.random()*gameElements.length)];
 
-    
-    for (let index = 0; index < btnComputerElement.length; index++) {
-        if (btnComputerElement[index].textContent.toLowerCase() === computerSelection) {
-            btnComputerElement[index].style.backgroundColor = "rgb(130,15,151)";
-        } else {
-            btnComputerElement[index].style.backgroundColor = "gray";
-        }        
-    }
+    setComputerBtnColor(computerSelection);
 
     if (playerSelection === computerSelection) {
         individualRoundResult.textContent = "It's a tie!";
+        individualRoundResult.style.color = "darkBlue";
     }
     if (playerSelection === "rock" && computerSelection === "paper") {
         counter.computer++
         individualRoundResult.textContent = "Computer wins the round! Paper beats the rock.";
+        individualRoundResult.style.color = "darkRed";
     }
     if (playerSelection === "rock" && computerSelection === "scissors") {
         counter.player++;
         individualRoundResult.textContent = "You win the round! The rock beats the scissors.";
+        individualRoundResult.style.color = "darkGreen";
     }
     if (playerSelection === "paper" && computerSelection === "rock") {
         counter.player++;
         individualRoundResult.textContent = "You win the round! Paper beats the rock.";
+        individualRoundResult.style.color = "darkGreen";
     }
     if (playerSelection === "paper" && computerSelection === "scissors") {
         counter.computer++;
         individualRoundResult.textContent = "Computer wins the round! The scissors beat paper.";
+        individualRoundResult.style.color = "darkRed";
     }
     if (playerSelection === "scissors" && computerSelection === "paper") {
         counter.player++;
         individualRoundResult.textContent = "You win the round! The scissors beat paper.";
+        individualRoundResult.style.color = "darkGreen";
     }
     if (playerSelection === "scissors" && computerSelection === "rock") {
         counter.computer++;
         individualRoundResult.textContent = "Computer wins the round; the rock beats the scissors.";
+        individualRoundResult.style.color = "darkRed";
     }
 
     playerScore.textContent = counter.player;
@@ -64,14 +67,25 @@ function playRound(e) {
 
     if (counter.player === 5 || counter.computer === 5) {
         getGameResult();
-        for (let index = 0; index < btnPlayerElement.length; index++) {
-            btnPlayerElement[index].disabled = true;
-        }
+        disablePlayerElements();
     }
 }
 
+function disablePlayerElements() {
+    rockPlayerElement.disabled = true;
+    paperPlayerElement.disabled = true;
+    scissorsPlayerElement.disabled = true;
+}
 
-btnPlayAgain.addEventListener("click", playAgain);
+function setComputerBtnColor(computerSelection) {
+    for (let index = 0; index < btnComputerElement.length; index++) {
+        if (btnComputerElement[index].textContent.toLowerCase() === computerSelection) {
+            btnComputerElement[index].style.backgroundColor = "rgb(130,15,151)";
+        } else {
+            btnComputerElement[index].style.backgroundColor = "gray";
+        }        
+    }
+}
 
 
 // To reset values
@@ -83,19 +97,23 @@ function playAgain() {
     individualRoundResult.textContent = "";
     finalResult.textContent = "";
 
-    for (let index = 0; index < btnPlayerElement.length; index++) {
-        btnPlayerElement[index].disabled = false;
+    for (let index = 0; index < btnComputerElement.length; index++) {
         btnComputerElement[index].style.backgroundColor = "gray";
     }
+    rockPlayerElement.disabled = false;
+    paperPlayerElement.disabled = false;
+    scissorsPlayerElement.disabled = false;
 }
 
 
 function getGameResult() {
     if (counter.player > counter.computer) {
         finalResult.textContent = "Congrats! You won the game!";
-    } else if (counter.player === counter.computer){
-        finalResult.textContent = "It's a tie.";
+        finalResult.style.color = "darkGreen";
+        finalResult.style.fontWeight = "bold";
     } else {
         finalResult.textContent = "Ups... You lost the game";
+        finalResult.style.color = "red";
+        finalResult.style.fontWeight = "bold";
     }
 }
